c3e71617021ca80c1fa6894285d6cab60d13fb39
Oscar Carod <oscar.carod@est.fib.upc.edu>
2016-10-14 14:25:13 +0200

    Primeras clases

--- app/src/main/java/com/pes12/pickanevent/view/MainActivity.java
+++ app/src/main/java/com/pes12/pickanevent/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.pes12.pickanevent;
+package com.pes12.pickanevent.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pes12.pickanevent.R;
 
 public class MainActivity extends AppCompatActivity {
 
