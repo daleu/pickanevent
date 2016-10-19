e6c1d112021ce7a07cbcf4f0dbe8300dd9c22116
David Aleu Mosegui <david.aleu@est.fib.upc.edu>
2016-10-19 23:59:20 +0200

    Afegit icona applicació i creat layout i activity de visualitzar esdeveniment

---
+++ app/src/main/java/com/pes12/pickanevent/view/VerInfoEvento.java
@@ -0,0 +1,15 @@
+package com.pes12.pickanevent.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pes12.pickanevent.R;
+
+public class VerInfoEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_info_evento);
+    }
+}
