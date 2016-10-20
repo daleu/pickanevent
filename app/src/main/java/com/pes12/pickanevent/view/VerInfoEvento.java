2f6402dd7cd3f04c23c2094c93c0847082fcc6ca
David Aleu Mosegui <david.aleu@est.fib.upc.edu>
2016-10-20 17:52:04 +0200

    Diferents canvis a la vista esdeveniment

--- app/src/main/java/com/pes12/pickanevent/view/VerInfoEvento.java
+++ app/src/main/java/com/pes12/pickanevent/view/VerInfoEvento.java
@@ -1,7 +1,9 @@
 package com.pes12.pickanevent.view;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.pes12.pickanevent.R;
 
@@ -11,5 +13,12 @@ public class VerInfoEvento extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_info_evento);
+
+        Typeface fontAwesomeFont = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
+
+        TextView clockIcon = (TextView) findViewById(R.id.clockIcon);
+        clockIcon.setTypeface(fontAwesomeFont);
+        TextView clockDollar = (TextView) findViewById(R.id.clockDollar);
+        clockDollar.setTypeface(fontAwesomeFont);
     }
 }
