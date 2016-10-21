4da3f07bb0c413547735fcc92d2c892e3f00d23a
David Aleu Mosegui <david.aleu@est.fib.upc.edu>
2016-10-21 10:37:01 +0200

    creats el manager i entity de evento

---
+++ app/src/main/java/com/pes12/pickanevent/business/Evento/EventoMGR.java
@@ -0,0 +1,96 @@
+package com.pes12.pickanevent.business.Evento;
+
+import android.app.Activity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.pes12.pickanevent.persistence.FirebaseSingleton;
+import com.pes12.pickanevent.persistence.entity.Evento.EventoEntity;
+import com.pes12.pickanevent.view.VerInfoEvento;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by aleue on 21/10/2016.
+ */
+
+public class EventoMGR {
+
+    private final FirebaseDatabase database;
+    private DatabaseReference bdRefEventos;
+    private static EventoMGR singleton;
+
+    public static EventoMGR getInstance()
+    {
+        if(singleton==null)
+        {
+            singleton= new EventoMGR();
+            return singleton;
+        }
+        else return singleton;
+    }
+
+    public EventoMGR () {
+
+        database = FirebaseSingleton.getInstance();
+        bdRefEventos = database.getReference("eventos");
+
+    }
+
+    public String crear(EventoEntity _entity)
+    {
+        bdRefEventos.orderByChild("nombreEvento").equalTo(_entity.getTitulo()).addListenerForSingleValueEvent(new ValueEventListener() {
+            EventoEntity ent;
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.getValue() != null) {
+                    System.out.println("YA EXISTE UN GRUPO CON ESE NOMBRE");
+                } else {
+                    DatabaseReference usuario = bdRefEventos.push();
+                    usuario.setValue(ent);
+                    usuario.getKey();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError arg0) {
+            }
+
+            public ValueEventListener setEntity (EventoEntity _ent)
+            {
+                ent=_ent;
+                return this;
+            }
+        }.setEntity(_entity));
+        return "";
+    }
+
+    public void getInfoGrupo(Activity _activity) {
+
+        bdRefEventos.orderByKey().addValueEventListener(new ValueEventListener() {
+            Map<String,EventoEntity> map = new LinkedHashMap<String,EventoEntity>();
+            VerInfoEvento activity;
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot evento : dataSnapshot.getChildren()) {
+                    map.put(evento.getKey(), evento.getValue(EventoEntity.class));
+                }
+                activity.mostrarInfoEvento(map);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("EEEERROOOOR");
+            }
+            public ValueEventListener setActivity (Activity _activity)
+            {
+                activity=(VerInfoEvento) _activity;
+                return this;
+            }
+        }.setActivity(_activity));
+    }
+}
