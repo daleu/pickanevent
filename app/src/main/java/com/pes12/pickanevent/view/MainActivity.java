c300da6bad4e695fcbe1b87c50fbfd3e58d0a881
David Aleu Mosegui <david.aleu@est.fib.upc.edu>
2016-10-25 14:55:15 +0200

    arreglat main

--- app/src/main/java/com/pes12/pickanevent/view/MainActivity.java
+++ app/src/main/java/com/pes12/pickanevent/view/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editar_evento);
+        setContentView(R.layout.activity_main);
 
 
 /*
