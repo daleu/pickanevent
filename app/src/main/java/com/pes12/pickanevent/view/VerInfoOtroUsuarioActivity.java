bbdaeab0a0b7bc80d2b698f7d1983425ca26f3a2
Jordi Jounou Beraza <jordi.jounou.beraza@est.fib.upc.edu>
2016-10-27 01:50:32 +0200

    Actualizacion en VerInfoOtroUsuario

--- app/src/main/java/com/pes12/pickanevent/view/VerInfoOtroUsuarioActivity.java
+++ app/src/main/java/com/pes12/pickanevent/view/VerInfoOtroUsuarioActivity.java
@@ -2,8 +2,10 @@ package com.pes12.pickanevent.view;
 
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
@@ -16,6 +18,7 @@ import com.pes12.pickanevent.business.Grupo.GrupoMGR;
 import com.pes12.pickanevent.business.Info;
 import com.pes12.pickanevent.business.MGRFactory;
 import com.pes12.pickanevent.business.Usuario.UsuarioMGR;
+import com.pes12.pickanevent.persistence.entity.Evento.EventoEntity;
 import com.pes12.pickanevent.persistence.entity.Usuario.UsuarioEntity;
 
 import java.util.List;
@@ -32,6 +35,8 @@ public class VerInfoOtroUsuarioActivity extends BaseActivity {
     String idGrupo;
     String idEvento;
 
+    List<Info> grupos;
+    List<Info> eventos;
 
     UsuarioMGR uMGR;
     GrupoMGR gMGR;
@@ -59,29 +64,54 @@ public class VerInfoOtroUsuarioActivity extends BaseActivity {
 
         //gMGR = new GrupoMGR().getInstance();
 
-        //List<Info> grupos = null; //llenar con todos los nombres de los grupos que sigue el ususario
+        grupos = null; //llenar con todos los nombres de los grupos que sigue el ususario
         //Crear variable y añadirla al Adapter que contendra todas las imagenes de los grupos
 
-        //List<Info> esdeveniments = null;//Crear variable con los nombres de todos los eventos que siga el usuario
+        eventos = null;//Crear variable con los nombres de todos los eventos que siga el usuario
         //Crear variable con todas las fechas de los eventos que sigue el usuario
         //Crear varibale con todas las imagenes de los eventos que sigue el usuario
 
-       // ListAdapter adaptadorGrupos = new CustomAdapterGruposActivity(this, grupos);
-        //ListAdapter adaptadorEsdeveniments = new CustomAdapterEsdevenimentsActivity(this, esdeveniments);
+        ListAdapter adaptadorGrupos = new CustomAdapterGruposActivity(this, grupos);
+        ListAdapter adaptadorEsdeveniments = new CustomAdapterEsdevenimentsActivity(this, eventos);
 
-        //ListView gruposUsuario = (ListView) findViewById(R.id.listaGruposUsuario);
-        //ListView esdevenimentsUsuario = (ListView) findViewById(R.id.listaEsdevenimentsUsuario);
+        ListView gruposUsuario = (ListView) findViewById(R.id.listaGruposUsuario);
+        ListView esdevenimentsUsuario = (ListView) findViewById(R.id.listaEsdevenimentsUsuario);
 
-        //gruposUsuario.setAdapter(adaptadorGrupos);
-        //esdevenimentsUsuario.setAdapter(adaptadorEsdeveniments);
+        gruposUsuario.setAdapter(adaptadorGrupos);
+        esdevenimentsUsuario.setAdapter(adaptadorEsdeveniments);
     }
 
     public void mostrarInfoUsuario(UsuarioEntity usuario) {
         nombre.setText(usuario.getNickname());
-        //foto.setImageResource(R.drawable.oso); //Cambiar con imagen de usuario
+        foto.setImageResource(R.drawable.oso); //Cambiar con imagen de usuario
         Map<String,Boolean> idGrupos = usuario.getIdGrupos();
         Map<String,Boolean> idEventos = usuario.getIdEventos();
-
+        for (int i=0; i < idGrupos.size(); ++i){
+            //rellenarListaGrupos();
+        }
+        for (int i=0; i < idEventos.size(); ++i){
+            //rellenarListaEventos()
+        }
     }
 
+    public void rellenarListaGrupos (EventoEntity grupo){
+        Info info = new Info(StringToBitMap(grupo.getImagen()),grupo.getTitulo(),null);
+        grupos.add(info);
+    }
+
+    public void rellenarListaEventos (EventoEntity evento) {
+        Info info = new Info(StringToBitMap(evento.getImagen()), evento.getTitulo(), evento.getHorario());
+        eventos.add(info);
+    }
+
+    private Bitmap StringToBitMap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
 }
