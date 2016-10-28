d69cdc3abeef23a0aa3af1556787d1ae4e5a3674
Victor Abel <victor.abel@est.fib.upc.edu>
2016-10-28 15:44:42 +0200

    afegits classes utils a les capes

---
+++ app/src/main/java/com/pes12/pickanevent/persistence/PersistenceUtils.java
@@ -0,0 +1,9 @@
+package com.pes12.pickanevent.persistence;
+
+/**
+ * Created by p4 on 28/10/16.
+ */
+
+public class PersistenceUtils {
+    //Aquesta classe incloura totes les funcions comunes que son utilitzades per mes de una classe
+}
